9e7309e54ff86a99dd3b88a9ed87fc4663b38fd2
Adam Jakubowski <ajakubo1@gmail.com>
2016-12-12 23:18:18 +0100

    Introduced some errors with file scopes

--- EnviroMonitorFrontend/app/index.js
+++ EnviroMonitorFrontend/app/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './app';
+import Root from './root';
 
 ReactDOM.render(
-    <App/>
+    <Root/>
     , document.getElementById('root')
 );
