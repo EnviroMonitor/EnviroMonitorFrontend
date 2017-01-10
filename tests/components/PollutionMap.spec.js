b1c57b5ca7b96cdc141f763fb37c484d2b0f2c13
Adam Jakubowski <ajakubo1@gmail.com>
2017-01-10 21:02:15 +0100

    Making sure tests are passing now

--- tests/components/PollutionMap.spec.js
+++ tests/components/PollutionMap.spec.js
@@ -2,12 +2,30 @@ import React from 'react';
 import { expect } from 'chai';
 import { shallow } from 'enzyme';
 import PollutionMap from '../../app/components/PollutionMap';
+import thunk from 'redux-thunk';
+import configureMockStore from 'redux-mock-store';
+import Immutable from 'immutable';
+
+const middlewares = [ thunk ];
+const mockStore = configureMockStore(middlewares);
 
 describe('<PollutionMap/>', () => {
     let wrapper;
+    let store;
+    const initialState = {
+        mapData: {
+            isFetching: false,
+            data: []
+        }
+    };
 
     beforeEach(() => {
-        wrapper = shallow(<PollutionMap/>);
+        store = mockStore(Immutable.fromJS(initialState));
+        wrapper = shallow(<PollutionMap/>, {
+            context: {
+                store
+            }
+        });
     });
 
     it('Making sure it has <Map/> element defined', () => {
