bdd38d6bf6e97e58015cd093b053aac849b08339
Adam Jakubowski <ajakubo1@gmail.com>
2017-01-22 19:00:19 +0100

    Splitting the tests for testing Component itself via mount, as well as checking if wrapper with TileLayer has been generated properly

--- tests/components/PollutionMap.spec.js
+++ tests/components/PollutionMap.spec.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { expect } from 'chai';
-import { shallow } from 'enzyme';
-import PollutionMap from '../../app/components/PollutionMap';
+import { mount, shallow } from 'enzyme';
+import { ConnectedPollutionMap } from '../../app/components/PollutionMap';
+import PollutionMapWrapper from '../../app/components/PollutionMap';
+import Immutable from 'immutable';
 import thunk from 'redux-thunk';
 import configureMockStore from 'redux-mock-store';
-import Immutable from 'immutable';
+import { METRIC_TYPES } from '../../app/helpers/mapMarkers';
+import { Map, Marker } from 'react-leaflet';
 
 const middlewares = [ thunk ];
 const mockStore = configureMockStore(middlewares);
@@ -15,24 +18,68 @@ describe('<PollutionMap/>', () => {
     const initialState = {
         mapData: {
             isFetching: false,
-            data: {}
+            data: [
+                {
+                    type: METRIC_TYPES.PM10,
+                    position: [52.229, 21.032],
+                    description: "This marker should be blue",
+                    value: null
+                },
+                {
+                    type: METRIC_TYPES.PM10,
+                    position: [52.217, 20.987],
+                    description: "This marker should be green",
+                    value: 25
+                },
+                {
+                    type: METRIC_TYPES.PM10,
+                    position: [52.236, 20.976],
+                    description: "This marker should be orange",
+                    value: 75
+                },
+                {
+                    type: METRIC_TYPES.PM10,
+                    position: [52.259, 21.045],
+                    description: "This marker should be red",
+                    value: 200
+                },
+            ]
         }
     };
 
     beforeEach(() => {
         store = mockStore(Immutable.fromJS(initialState));
-        wrapper = shallow(<PollutionMap/>, {
-            context: {
-                store
-            }
+    });
+
+    describe('Testing the map element itself', () => {
+        beforeEach(() => {
+            wrapper = mount(<ConnectedPollutionMap/>, {
+                context: {
+                    store
+                }
+            });
+        });
+
+        it('Making sure it has <Map/> element defined', () => {
+            expect(wrapper.find(Map)).to.have.length(1);
+        });
+
+        it('Making sure it has the right amount of <Marker/> elements defined', () => {
+            expect(wrapper.find('Marker')).to.have.length(4);
         });
     });
 
-    it('Making sure it has <Map/> element defined', () => {
-        expect(wrapper.find('Map')).to.exist;
-    });
+    describe('Testing the wrapper', () => {
+        beforeEach(() => {
+            wrapper = shallow(<PollutionMapWrapper/>, {
+                context: {
+                    store
+                }
+            });
+        });
 
-    it('Making sure it has <TileLayer/> element defined', () => {
-        expect(wrapper.find('TileLayer')).to.exist;
+        it('renders the wrapper correctly', () => {
+            expect(wrapper.find(ConnectedPollutionMap)).to.have.length(1);
+        });
     });
 });
